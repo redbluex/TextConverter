046c705f2a4ee1df16bde3db3e85e57f19aef7c2
redbluex <redbluedev@gmail.com>
2018-04-04 14:20:34 +0200

    finall version

--- src/textconvert/TextConvert.java
+++ src/textconvert/TextConvert.java
@@ -5,6 +5,11 @@
  */
 package textconvert;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 /**
  *
  * @author rob
@@ -14,8 +19,29 @@ public class TextConvert {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    public static void main(String[] args) throws FileNotFoundException {
+        String path;
+        Scanner fromUser = new Scanner(System.in);
+        System.out.println("Podaj nazwe pliku wejsciowego (znajduje sie w dokumentach): ");
+        path = fromUser.nextLine();
+        Scanner reader = new Scanner(new File("C:\\Users\\rob\\Documents\\"+path));
+        System.out.println("Podaj sciezke pliku wyjsciowego: ");
+        path = fromUser.nextLine();
+        PrintWriter writeFile = new PrintWriter("C:\\Users\\rob\\Documents\\"+path);
+        System.out.println("Podaj sciezke pliku wyjsciowego - drugiego");
+        path = fromUser.nextLine();
+        PrintWriter writeFile2 = new PrintWriter("C:\\Users\\rob\\Documents\\"+path);
+        
+        while(reader.hasNext()){
+        String stringReader = reader.nextLine();
+        String[] parts = stringReader.split("	");
+        writeFile.println(parts[1]);
+        writeFile2.println(parts[2]);
+        System.out.println(parts[2]);
+        }
+        
+        writeFile.close();
+        writeFile2.close();
     }
     
 }
